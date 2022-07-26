682ba299b689f814095c10187362c8b5e93b0b4f
Ice <icekong0906@gmail.com>
2022-07-26 19:20:02 +0700

    24 CM

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
+        child: Icon(Icons.add),
       ),
     );
   }
