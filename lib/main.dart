f41a3673613ab4400e2ce0ccaa437af51b48a467
Ice <icekong0906@gmail.com>
2022-07-26 19:02:01 +0700

    21 CM

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,44 @@ void main() {
   runApp(MyApp());
 }
 
-// สร้าง widged
+//// สร้าง widged
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("ยินดีต้อนรับ"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text("สวัสดีค่ะทุกคน"),
-              Text("Hello Dart"),
-              Text("Hello Flutter")
-            ],
-          ),
+      home: MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.lightGreen),
+    );
+  }
+}
+
+/////////////////////////////////////////////////////////////////////
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ยินดีต้อนรับ"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("สวัสดีค่ะทุกคน"),
+            Text("Hello Dart"),
+            Text("Hello Flutter")
+          ],
         ),
       ),
-      theme: ThemeData(primarySwatch: Colors.lightGreen),
     );
   }
 }
